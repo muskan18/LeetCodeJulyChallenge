03da934903f1590ed772b194e1676353fb7f00f7
Muskan Bansal <44235014+muskan18@users.noreply.github.com>
2020-07-19 13:56:57 +0530

    Day 19

---
+++ Addbinary.java
@@ -0,0 +1,41 @@
+/*
+Given two binary strings, return their sum (also a binary string).
+
+The input strings are both non-empty and contains only characters 1 or 0.
+*/
+StringBuilder sb = new StringBuilder();
+ 
+    int i=a.length()-1;
+    int j=b.length()-1;
+ 
+    int carry = 0;
+ 
+    while(i>=0 || j>=0){
+        int sum=0;
+ 
+        if(i>=0 && a.charAt(i)=='1'){
+            sum++;    
+        }
+ 
+        if(j>=0 && b.charAt(j)=='1'){
+            sum++;
+        }
+ 
+        sum += carry;
+ 
+        if(sum>=2){
+            carry=1;
+        }else{
+            carry=0;
+        }
+ 
+        sb.insert(0,  (char) ((sum%2) + '0'));
+ 
+        i--;
+        j--;
+    }
+ 
+    if(carry==1)
+        sb.insert(0, '1');
+ 
+    return sb.toString();
